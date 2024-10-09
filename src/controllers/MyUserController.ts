6b76f72de9323836142ee43616b248485c526534
gitony0101 <53283254+gitony0101@users.noreply.github.com>
2024-10-09 00:05:32 -0300

    user update files OK full
    Signed-off-by: gitony0101 <53283254+gitony0101@users.noreply.github.com>

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -1,32 +1,54 @@
 import { Request, Response, NextFunction } from 'express';
 import User from '../models/user';
 
-// 创建用户
+// Get current user
+const getCurrentUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const currentUser = await User.findOne({ _id: req.userId });
+    if (!currentUser) {
+      res.status(404).json({ message: 'User not found' });
+      return;
+    }
+    res.json(currentUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'Something went wrong' });
+    next(error); // Pass the error to the error-handling middleware
+  }
+};
+
+// Create a new user
 const createCurrentUser = async (
   req: Request,
   res: Response,
   next: NextFunction,
 ): Promise<void> => {
   try {
-    // 1. 检查用户是否存在
+    // 1. Check if the user already exists
     const { auth0Id } = req.body;
     const existingUser = await User.findOne({ auth0Id });
     if (existingUser) {
-      res.status(200).json(existingUser); // 返回已存在的用户对象
+      res.status(200).json(existingUser); // Return existing user object
       return;
     }
-    // 2. 如果不存在则创建新用户
+
+    // 2. If user does not exist, create a new one
     const newUser = new User(req.body);
     await newUser.save();
-    // 3. 返回新创建的用户对象给客户端
+
+    // 3. Return the newly created user object to the client
     res.status(201).json(newUser.toObject());
   } catch (error) {
     console.log(error);
-    next(error); // 调用 next 将错误传递给错误处理程序
+    next(error); // Pass the error to the error-handling middleware
   }
 };
 
-// 更新用户
+// Update the user
 const updateCurrentUser = async (
   req: Request,
   res: Response,
@@ -40,18 +62,18 @@ const updateCurrentUser = async (
       return;
     }
 
-    // 更新用户信息
+    // Update user information
     user.name = name;
     user.addressLine1 = addressLine1;
     user.country = country;
     user.city = city;
     await user.save();
 
-    res.status(200).json(user); // 返回更新后的用户对象
+    res.status(200).json(user); // Return updated user object
   } catch (error) {
     console.log(error);
-    next(error); // 调用 next 将错误传递给错误处理程序
+    next(error); // Pass the error to the error-handling middleware
   }
 };
 
-export default { createCurrentUser, updateCurrentUser };
+export default { getCurrentUser, createCurrentUser, updateCurrentUser };
