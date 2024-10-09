daf19356f7423964bd153b44843720d6db5b4625
gitony0101 <53283254+gitony0101@users.noreply.github.com>
2024-10-09 20:36:26 -0300

    update
    Signed-off-by: gitony0101 <53283254+gitony0101@users.noreply.github.com>

--- src/routes/MyRestaurantRoute.ts
+++ src/routes/MyRestaurantRoute.ts
@@ -1,18 +1,23 @@
 import express from 'express';
 import multer from 'multer';
 import MyRestaurantController from '../controllers/MyRestaurantController';
+import { jwtCheck } from '../middleware/jwtCheck.middleware';
+import { jwtParse } from '../middleware/jwtParse.middleware';
 
 const router = express.Router();
 
+// image configuration
 const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 限制文件大小为 5MB
+  limits: { fileSize: 5 * 1024 * 1024 },
 });
 
 // api/my/restaurant
 router.post(
   '/',
+  jwtCheck,
+  jwtParse,
   upload.single('imageFile'),
   MyRestaurantController.createMyRestaurant,
 );
