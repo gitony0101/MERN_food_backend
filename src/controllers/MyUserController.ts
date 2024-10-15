ac1dbdc0f327471544cc1b1abdfabbaa82b2e40c
gitony0101 <53283254+gitony0101@users.noreply.github.com>
2024-10-15 06:56:52 -0300

    Revert back to 3a95ab0 commit

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -1,79 +1,24 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response } from 'express';
 import User from '../models/user';
 
-// Get current user
-const getCurrentUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-): Promise<void> => {
+const createCurrentUser = async (req: Request, res: Response) => {
   try {
-    const currentUser = await User.findOne({ _id: req.userId });
-    if (!currentUser) {
-      res.status(404).json({ message: 'User not found' });
-      return;
-    }
-    res.json(currentUser);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: 'Something went wrong' });
-    next(error); // Pass the error to the error-handling middleware
-  }
-};
-
-// Create a new user
-const createCurrentUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-): Promise<void> => {
-  try {
-    // 1. Check if the user already exists
+    // 1. Check if the user exists
     const { auth0Id } = req.body;
     const existingUser = await User.findOne({ auth0Id });
     if (existingUser) {
-      res.status(200).json(existingUser); // Return existing user object
+      res.status(200).send();
       return;
     }
-
-    // 2. If user does not exist, create a new one
+    // 2. Create the user if it doesn't exist
     const newUser = new User(req.body);
     await newUser.save();
-
-    // 3. Return the newly created user object to the client
+    // 3. return the user object to the calling client
     res.status(201).json(newUser.toObject());
   } catch (error) {
     console.log(error);
-    next(error); // Pass the error to the error-handling middleware
+    res.status(500).json({ message: 'Error creating user' });
   }
 };
 
-// Update the user
-const updateCurrentUser = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-): Promise<void> => {
-  try {
-    const { name, addressLine1, country, city } = req.body;
-    const user = await User.findById(req.userId);
-    if (!user) {
-      res.status(404).json({ message: 'User not found.' });
-      return;
-    }
-
-    // Update user information
-    user.name = name;
-    user.addressLine1 = addressLine1;
-    user.country = country;
-    user.city = city;
-    await user.save();
-
-    res.status(200).json(user); // Return updated user object
-  } catch (error) {
-    console.log(error);
-    next(error); // Pass the error to the error-handling middleware
-  }
-};
-
-export default { getCurrentUser, createCurrentUser, updateCurrentUser };
+export default { createCurrentUser };
--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,21 +1,9 @@
 import express from 'express';
 import MyUserController from '../controllers/MyUserController';
-import { jwtCheck } from '../middleware/jwtCheck.middleware';
-import { jwtParse } from '../middleware/jwtParse.middleware';
-import { validateMyUserRequest } from '../middleware/validation';
 
 const router = express.Router();
 
 // api/my.user
-router.get('/', jwtCheck, jwtParse, MyUserController.getCurrentUser);
-
-router.post('/', jwtCheck, MyUserController.createCurrentUser);
-router.put(
-  '/',
-  jwtCheck,
-  jwtParse,
-  validateMyUserRequest,
-  MyUserController.updateCurrentUser,
-);
+router.post('/', MyUserController.createCurrentUser);
 
 export default router;
